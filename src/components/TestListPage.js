7dfd671e6a2223cd6e8c36995c88546726b008ee
Manoj Nepali <manoj.nepali@midastechnologies.com.np>
2025-02-09 16:03:44 +0545

    changes in features

--- src/components/TestListPage.js
+++ src/components/TestListPage.js
@@ -34,7 +34,7 @@ const TestListPage = () => {
         <h1 className="text-3xl font-bold mb-8">Select a Cambridge Test</h1>
 
         {/* Test Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6">
           {passages.map((test) => (
             <div
               key={test.id}
